38fe85a505f48093501334af0d07c7422ce0812d
Remco Stoeten <remcostoeten@hotmail.com>
2023-05-24 14:17:16 +0200

    add future github retrieves

---
+++ lib/types.ts
@@ -0,0 +1,22 @@
+export interface user {
+	name: string;
+	user: string;
+}
+
+export interface document {
+	id: string;
+	title: string;
+	content: string;
+  }
+
+  export interface HomeProps {
+	user: user;
+	documents: DocumentData[]; // Verander 'document' naar 'DocumentData'
+  }
+  
+export interface  DocumentData {
+	id: string;
+	title: string;
+	content: string;
+  };
+
