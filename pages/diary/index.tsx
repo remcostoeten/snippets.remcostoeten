fa707d2d7ed4968e4eb22282e0d9412db5132ee5
Remco Stoeten <remcostoeten@hotmail.com>
2023-05-26 15:45:08 +0200

    Change path

--- pages/diary/index.tsx
+++ pages/diary/index.tsx
@@ -1,22 +1,22 @@
-import { useContext } from "react";
-import { AuthContext } from "@/lib/AuthContext";
-import { User } from "@/lib/types";
-import { EntryForm } from "../../components/diary/EntryForm";
-import { EntriesList } from "../../components/diary/EntriesList";
+import { useContext } from 'react';
+import { AuthContext } from '@/lib/AuthContext';
+import { User } from '@/lib/types';
+import { EntryForm } from '@/components/diary/EntryForm';
+import { EntriesList } from '@/components/diary/EntriesList';
 
 export default function Index() {
-  const { currentUser } = useContext(AuthContext);
+	const { currentUser } = useContext(AuthContext);
 
-  if (!currentUser) {
-    return <div>Please log in to view this page.</div>;
-  }
+	if (!currentUser) {
+		return <div>Please log in to view this page.</div>;
+	}
 
-  const user: User = currentUser;
+	const user: User = currentUser;
 
-  return (
-    <div>
-      <EntryForm user={user} />
-      <EntriesList user={user} />
-    </div>
-  );
+	return (
+		<div>
+			<EntryForm user={user} />
+			<EntriesList user={user} />
+		</div>
+	);
 }
--- pages/todos.tsx
+++ pages/todos.tsx
@@ -1,3 +1,4 @@
+
 import { useState } from 'react';
 import Link from 'next/link';
 import { firestore, handleAddDocument, handleToggleDocument } from '../lib/firebase';
