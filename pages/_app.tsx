9fec1d2c20208780e8ec49dc95091db87778eeba
remco stoeten <remcostoeten@hotmail.com>
2023-05-26 03:43:59 +0200

    Fix build errors.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,36 @@
 import type { AppProps } from 'next/app';
+import { useState, useEffect } from 'react';
 import '../styles/globals.scss';
 import Sidebar from '@/components/Sidebar';
 import TopNotice from '@/components/ui-elements/TopNotice';
-function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<>
-			<TopNotice />
-			<div className="flex">
-				<Sidebar />
-				<main className="main-content flex flex-col flex-1 h-screen bg-white rounded-lg p-4 w-95">
-					<Component {...pageProps} />
-				</main>
-			</div>
+import { AuthProvider } from '@/lib/AuthContext';
 
-			<Component {...pageProps} />
-		</>
-	);
+function MyApp({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <div>Preloader of Skeleton-component...</div>;
+  }
+
+  return (
+    <AuthProvider>
+      <TopNotice />
+      <div className="flex">
+        <Sidebar />
+        <main className="main-content flex flex-col flex-1 h-screen bg-white rounded-lg p-4 w-95">
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </AuthProvider>
+  );
 }
 
 export default MyApp;
--- pages/diary/index.tsx
+++ pages/diary/index.tsx
@@ -1,11 +1,22 @@
-import { EntryForm } from '@/components/Diary/EntryForm';
-import { EntriesList } from '@/components/Diary/EntriesList';
+import { useContext } from "react";
+import { AuthContext } from "@/lib/AuthContext";
+import { EntryForm } from "@/components/diary/EntryForm";
+import { EntriesList } from "@/components/diary/EntriesList";
+import { User } from "@/lib/types";
 
-export default function index() {
-	return (
-		<div>
-			<EntryForm />
-			<EntriesList />
-		</div>
-	);
+export default function Index() {
+  const { currentUser } = useContext(AuthContext);
+
+  if (!currentUser) {
+    return <div>Please log in to view this page.</div>;
+  }
+
+  const user: User = currentUser;
+
+  return (
+    <div>
+      <EntryForm user={user} />
+      <EntriesList user={user} />
+    </div>
+  );
 }
