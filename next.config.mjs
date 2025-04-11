65c034705a2e366f36d2e5c912f5ac1408626be8
Remco Stoeten <remcostoeten@hotmail.com>
2025-04-11 23:51:38 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,44 +1,47 @@
-import { createMDX } from 'fumadocs-mdx/next'
+import { createMDX } from "fumadocs-mdx/next";
 
-const withMDX = createMDX()
+const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
-    reactStrictMode: true,
-    poweredByHeader: false,
-    compress: true,
-    images: {
-        formats: ['image/avif', 'image/webp'],
-        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-        minimumCacheTTL: 60
-    },
-    headers: async () => [
+  reactStrictMode: true,
+  poweredByHeader: false,
+  compress: true,
+  images: {
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    minimumCacheTTL: 60,
+  },
+  headers: async () => [
+    {
+      source: "/:path*",
+      headers: [
         {
-            source: '/:path*',
-            headers: [
-                {
-                    key: 'X-DNS-Prefetch-Control',
-                    value: 'on'
-                },
-                {
-                    key: 'X-XSS-Protection',
-                    value: '1; mode=block'
-                },
-                {
-                    key: 'X-Frame-Options',
-                    value: 'SAMEORIGIN'
-                },
-                {
-                    key: 'X-Content-Type-Options',
-                    value: 'nosniff'
-                },
-                {
-                    key: 'Referrer-Policy',
-                    value: 'strict-origin-when-cross-origin'
-                }
-            ]
-        }
-    ]
-}
+          key: "X-DNS-Prefetch-Control",
+          value: "on",
+        },
+        {
+          key: "X-XSS-Protection",
+          value: "1; mode=block",
+        },
+        {
+          key: "X-Frame-Options",
+          value: "SAMEORIGIN",
+        },
+        {
+          key: "X-Content-Type-Options",
+          value: "nosniff",
+        },
+        {
+          key: "Referrer-Policy",
+          value: "strict-origin-when-cross-origin",
+        },
+      ],
+    },
+  ],
+  experimental: {
+    esmExternals: "loose",
+  },
+};
 
-export default withMDX(config)
+export default withMDX(config);
