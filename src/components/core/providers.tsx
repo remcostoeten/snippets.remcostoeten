d79344176953367877cd9a3d094c117b14d36896
Remco Stoeten <remcostoeten@hotmail.com>
2025-09-11 16:26:18 +0200

    feat: add analytics

--- src/components/core/providers.tsx
+++ src/components/core/providers.tsx
@@ -1,22 +1,27 @@
 import { ThemeProvider } from "next-themes";
 import { RootProvider } from 'fumadocs-ui/provider';
 import { TooltipProvider } from '../../shared/ui';
+import { Analytics } from "@vercel/analytics/next"
 
 type TProps = {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export function Providers({ children }: TProps) {
-    return (
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem={false}
-            disableTransitionOnChange
-        >
-            <TooltipProvider>
-                <RootProvider>{children}</RootProvider>
-            </TooltipProvider>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem={false}
+      disableTransitionOnChange
+    >
+      <TooltipProvider>
+        <RootProvider>{children}
+          <Analytics />
+        </RootProvider>
+      </TooltipProvider>
+
+    </ThemeProvider>
+
+  );
 }
